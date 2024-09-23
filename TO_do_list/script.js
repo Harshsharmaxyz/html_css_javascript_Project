8a82a2e161cc5aac1846b99f60f9a666d8814732
Harsh Sharma xyz <hs5586528@gmail.com>
2024-09-23 18:11:33 +0530

    todolist

---
+++ TO_do_list/script.js
@@ -0,0 +1,11 @@
+const inputBox = document.getElementById('inputBox')
+const addBtn = document.getElementById('addBtn ')
+const todoList = document.getElementById('todoList')
+
+addBtn.addEventListener('click',addTodo);
+
+const addTo alert(message?:any): void
+
+const addTodo = () =>{
+    alert ("Hello");
+}
