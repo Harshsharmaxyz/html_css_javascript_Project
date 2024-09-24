919c9fd9bf90e38956d7b48d1bf031ddb8fc8ab5
Harsh Sharma xyz <hs5586528@gmail.com>
2024-09-24 15:41:43 +0530

    new message

--- TO_do_list/script.js
+++ TO_do_list/script.js
@@ -2,10 +2,11 @@ const inputBox = document.getElementById('inputBox')
 const addBtn = document.getElementById('addBtn ')
 const todoList = document.getElementById('todoList')
 
-addBtn.addEventListener('click',addTodo);
+const addTodo = ()=>{
+  const inputText = inputBox.ariaValueMax.trim();
+  if(inputText.length <= 0){
+    alert("you")
 
-const addTo alert(message?:any): void
-
-const addTodo = () =>{
-    alert ("Hello");
+  }
 }
+addBtn.addEventListener('click',addTodo);
